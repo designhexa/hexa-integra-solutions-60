dc6f4847a28c5bfeaccb543af6d5df43e6344fa3
designhexa <157108416+designhexa@users.noreply.github.com>
2025-08-11 16:53:14 +0700

    Update ProposalSlider.tsx

--- src/components/ProposalSlider.tsx
+++ src/components/ProposalSlider.tsx
@@ -350,7 +350,7 @@ const ProposalSlider = () => {
         </div>
 
         {/* Indikator Bulatan dengan posisi tetap */}
-        <div className="flex justify-center gap-3 mt-24">
+        <div className="flex justify-center gap-3 mt-30">
           {slides.map((_, index) => (
             <button
               key={index}
